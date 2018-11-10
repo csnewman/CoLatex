14088698ba1fba71044650e8bb2544b5baa91929
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-11-10 13:26:15 +0000

    Add web template

---
+++ CoLatex/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoLatex.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
