a5e638ad8b56a5d9c9c000200253f906a7d18bb3
Chandler Newman <chandler2newman@hotmail.co.uk>
2018-11-10 22:36:57 +0000

    Add empty file creation

---
+++ CoLatex/Projects/CreateResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoLatex.Projects
+{
+    public class CreateResponseModel
+    {
+        public bool Success { get; set; }
+        public FileModel File { get; set; }
+    }
+}
--- CoLatex/Projects/ProjectsController.cs
+++ CoLatex/Projects/ProjectsController.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -112,6 +112,35 @@ namespace CoLatex.Projects
             await _projectManager.OnFileAdded(model.ProjectId, model.Path);
 
             return new UploadResponseModel
+            {
+                Success = true,
+                File = _projectManager.GetFileModel(_projectManager.GetFilePath(model.ProjectId, path))
+            };
+        }
+
+        [HttpPost("create")]
+        public async Task<CreateResponseModel> CreateResourceAsync(CreateModel model)
+        {
+            ClaimsPrincipal principal = HttpContext.User;
+            string username = principal.FindFirstValue("username");
+
+            ProjectDbModel dbModel = await _projectRepository.GetProject(model.ProjectId);
+
+            if (!(string.Equals(dbModel.Owner, username) ||
+                  (dbModel.Collaborators != null && dbModel.Collaborators.Contains(username))))
+            {
+                return new CreateResponseModel
+                {
+                    Success = false
+                };
+            }
+
+            string path = Path.GetFullPath(_projectManager.GetFilePath(model.ProjectId, model.Path));
+            new FileStream(path, FileMode.Create).Dispose();
+
+            await _projectManager.OnFileAdded(model.ProjectId, model.Path);
+
+            return new CreateResponseModel
             {
                 Success = true,
                 File = _projectManager.GetFileModel(_projectManager.GetFilePath(model.ProjectId, path))
