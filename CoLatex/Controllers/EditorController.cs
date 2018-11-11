c0610ea6c12573c83503ef822e486cc4a5995fd4
Edward Cooper <edward.cooper100@gmail.com>
2018-11-11 06:19:07 +0000

    Create editor controller

---
+++ CoLatex/Controllers/EditorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoLatex.Controllers
+{
+    public class EditorController : Controller
+    {
+        public IActionResult Index(string project)
+        {
+            return View();
+        }
+    }
+}
