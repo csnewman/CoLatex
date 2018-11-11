a35100188004ff1219e4b88757d7a672f7bf60e0
Edward Cooper <edward.cooper100@gmail.com>
2018-11-11 01:39:42 +0000

    add jwt ajax support

--- CoLatex/wwwroot/js/dashboard.js
+++ CoLatex/wwwroot/js/dashboard.js
@@ -1,5 +1,11 @@
 ﻿$(document).ready(function () {
-    alert("HI");
-    $.getJSON('/api/projects/list', function (data) {
+    var jwtToken = "hi";
+    $.ajax({
+        url: "/api/projects/list",
+        dataType: 'json',
+        success: function (data, status) {
+            console.log(data);
+        },
+        beforeSend: function (xhr, settings) { xhr.setRequestHeader('Authorization', 'Bearer ' + jwtToken); } //set tokenString before send
     });
 });
